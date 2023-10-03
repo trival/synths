d4a2a31c935a752673b808403f8fb4b0b9d3ca85
Thomas Gorny <thomas.gorny@pm.me>
2023-10-03 21:18:31 +0200

    remove more traces of fable

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
 	clearScreen: false,
 	plugins: [
-		/* 
+		/*
     Uncomment the following line to enable solid-devtools.
     For more info see https://github.com/thetarnav/solid-devtools/tree/main/packages/extension#readme
     */
@@ -14,18 +14,5 @@ export default defineConfig({
 	],
 	server: {
 		port: 3000,
-		watch: {
-			ignored: [
-				"**/*.fs", // Don't watch F# files
-			],
-		},
 	},
-	build: {
-		target: "esnext",
-	},
-	esbuild: {
-		loader: "jsx",
-		include: ["src/**/*.js"],
-		exclude: [],
-	},
-});
+})
